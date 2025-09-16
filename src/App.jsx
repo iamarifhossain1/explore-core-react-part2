f62a8d8bd2c2c2a2dd7eee41f444f39cf60aff25
iamarifhossain1 <sporshoahmed2667@gmail.com>
2025-09-16 16:18:11 +0600

    event-handler

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,31 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  
+  function handleClick() {
+    alert('You have clicked me')
+  }
+
+const handleClick3 = () => {
+  alert('Alert Number 3')
+}
+
+const handleNum = (num) => {
+  const newNum = num + 5;
+  alert(newNum);
+}
+
   return (
     <>
       
       <h1>Vite + React</h1>
+      <button onClick={handleClick}>Click Me</button>
+      <button onClick={function handleClick2() {
+        alert('You have clicked me2')
+      }}>Click Me2</button>
+
+      <button onClick={handleClick3}>Click Me3</button>
+      <button onClick={() => alert('click 4') }>Click Me4</button>
+      <button onClick={() => handleNum(10)}>Click Add 5</button>
     </>
   )
 }
